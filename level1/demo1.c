e7603b46853d3ce05fac7e4ae987a0e927de8326
Bushra Fathima <f.bushra1006@gmail.com>
2026-03-11 13:59:57 +0530

    Completed demo1

---
+++ level1/demo1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+void input(int n, float arr[n]) // entering the array elements
+{
+    printf("Enter elements : \n");
+    for (int i = 0; i < n; i++)
+        scanf("%f", &arr[i]);
+}
+int find(int n, float arr[n]) // finding max value and index
+{
+    int max_index = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > arr[max_index])
+            max_index = i;
+    }
+    return max_index;
+}
+void output(float arr[], int max_index) // displaying result
+{
+    printf("Maximum value = %.2f\n", arr[max_index]);
+    printf("Maximum index value =%d\n", max_index);
+}
+int main()
+{
+    int n, max_index;
+    printf("Enter the number of elements:\n");
+    scanf("%d", &n);
+    float arr[n];
+    input(n, arr); // calling the functions
+    max_index = find(n, arr);
+    output(arr, max_index);
+    return 0;
+}
